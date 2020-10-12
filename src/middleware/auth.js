c089bab1790c63fa3164d797bec432aad8c5ec95
Akiza Rojo <akizadyanrojo@gmail.com>
2020-10-12 12:10:38 +0800

    heroku

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,12 +3,14 @@ const User = require('../models/user')
 
 const auth = async (req, res , next) => 
 {
+   
    try{ 
     const token = req.header('Authorization').replace('Bearer ', '') 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    console.log(decoded._id)
+    
+  
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
-
+    
     if (!user) {
         throw new Error
     }
@@ -17,7 +19,6 @@ const auth = async (req, res , next) =>
     next()
 
    }catch (e){
-       console.log(e)
     res.status(401).send({ error: 'Please authenticate'})
    }
 }
